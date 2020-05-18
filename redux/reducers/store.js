f6ca22d7513da0b37ffa6f3d9a3b6d0f278b90eb
Krishna Kumar <krishna.kumar@publicissapient.com>
2020-05-19 01:48:34 +0530

    Redux devtools integrated

--- redux/reducers/store.js
+++ redux/reducers/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
 import { createWrapper } from 'next-redux-wrapper'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './'
 
-const makeStore = () => createStore(rootReducer)
+const makeStore = () => createStore(rootReducer, composeWithDevTools())
 export const wrapper = createWrapper(makeStore, { debug: true })
